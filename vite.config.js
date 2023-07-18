f6a9bcb4badf8a8546b13badba502dab5a68c963
JhonanCaleb <jhonancalebm@gmail.com>
2023-07-18 11:06:53 -0500

    base update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/movie-search-react/",
+  base: "https://jhonancaleb.github.io/movie-search-react",
 });
