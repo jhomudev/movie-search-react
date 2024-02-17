420fb2e42eeacd03e1421587a9a962c6b5d991bc
JhonanCaleb <jhonancalebm@gmail.com>
2024-02-17 11:01:38 -0500

    some fixes

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,32 +1,32 @@
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState, useRef } from 'react'
 
-export function useSearch() {
-  const [search, updateSearch] = useState("");
-  const [error, setError] = useState(null);
+export function useSearch () {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
 
-  const isFirstInput = useRef(true);
+  const isFirstInput = useRef(true)
 
   useEffect(() => {
     if (isFirstInput.current) {
-      isFirstInput.current = search === "";
-      return;
+      isFirstInput.current = search === ''
+      return
     }
 
-    if (search === "") {
-      setError("Escriba la pelicula a buscar");
-      return;
+    if (search === '') {
+      setError('Escriba la pelicula a buscar')
+      return
     }
     if (search.length < 3) {
-      setError("Escriba como mínimo 3 caracteres");
-      return;
+      setError('Escriba como mínimo 3 caracteres')
+      return
     }
     if (search.match(/^\d+$/)) {
-      setError("No puede buscar con números");
-      return;
+      setError('No puede buscar con números')
+      return
     }
 
-    setError(null);
-  }, [search]);
+    setError(null)
+  }, [search])
 
-  return { search, updateSearch, error };
+  return { search, updateSearch, error }
 }
--- src/services/movies.js
+++ src/services/movies.js
@@ -1,23 +1,23 @@
-const API_KEY = "d74c8576";
+const API_KEY = 'd74c8576'
 
-export async function searchMovies({ search }) {
-  if (search == "") return null;
+export async function searchMovies ({ search }) {
+  if (search === '') return null
 
   try {
     const response = await fetch(
       `https://www.omdbapi.com/?apikey=${API_KEY}&s=${search}`
-    );
-    const json = await response.json();
+    )
+    const json = await response.json()
 
-    const movies = json.Search;
+    const movies = json.Search
 
     return movies?.map((movie) => ({
       id: movie.imdbID,
       title: movie.Title,
       year: movie.Year,
-      poster: movie.Poster,
-    }));
+      poster: movie.Poster
+    }))
   } catch (error) {
-    throw new Error("Errro en la busqueda");
+    throw new Error('Errro en la busqueda')
   }
 }
